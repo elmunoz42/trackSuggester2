7faf8e74aafa757419ae76005955c1cf0652afad
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-01-13 12:15:51 -0800

    add pathFinder function javascript not running.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,29 @@
 ////////////////Business End////////////////////
+function greetAndventurer(name) {
 
 
+
+}
+function pathFinder(inputA,inputB) {
+  var inputA = prompt("A");
+  var inputB = prompt("B");
+  var result;
+  if (inputA === "A3" || inputB === "B3") {
+    alert("you should look into C#.")
+  }
+  else if (inputA === "B1" || inputA === "B3" || input A === "B4") {
+    alert("you should look into Java. NOT THE SAME AS JAVASCRIPT!!!")
+  }
+  else if (inputA === "A2" || inputA === "A3" || inputB === "B1") {
+    alert("you should look into PHP.")
+  }
+  else if (inputA === "A1" || inputA === "A2" || inputA === "A3" || inputB === "B1" || inputB === "B2" || inputB === "B4")
+  } else {alert("I hate/love jQuery!!!")}
+
+function suggestedTraining(inputC) {
+
+
+}
 
 
 
