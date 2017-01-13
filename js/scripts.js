c789234fd4684d85e3564a9d916d8fbd4de7bb72
Carlos Munoz Kampff <elmunoz42@gmail.com>
2017-01-13 09:55:15 -0800

    resolves linking issues, adds bootstrap, jQuery, script, index.html, etc.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+////////////////Business End////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////////User Interface//////////////////////
+$(document).ready(function() {
+
+alert("Hello Budding Programmer! Please fill out this form and you can learn how to make pop ups like this one, and so much more!")
+
+
+
+
+
+
+})
